5d734aaa01d8465ba412f8e830e33c6dac1fc684
Sebastian Mueller <sebastian.mueller@yworks.com>
2020-08-23 13:05:56 +0200

    Dynamically loading Panels via Webpack Context

--- src/components/DetailPanel.vue
+++ src/components/DetailPanel.vue
@@ -1,82 +1,42 @@
 <template>
-  <v-card v-if="nodeIs('Paper')" flat>
-    <PaperPanel :value="model"></PaperPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Author')" flat>
-    <AuthorPanel :value="model"></AuthorPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Affiliation')" flat>
-    <AffiliationPanel :value="model"></AffiliationPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('GeneSymbol')" flat>
-    <GenePanel :value="model"></GenePanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Patent')" flat>
-    <PatentPanel :value="model"></PatentPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('ClinicalTrial')" flat>
-    <ClinicalTrialPanel :value="model"></ClinicalTrialPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Pathway')" flat>
-    <PathwayPanel :value="model"></PathwayPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Protein')" flat>
-    <ProteinPanel :value="model"></ProteinPanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('GtexDetailedTissue')" flat>
-    <TissuePanel :value="model"></TissuePanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Disease')" flat>
-    <DiseasePanel :value="model"></DiseasePanel>
-  </v-card>
-  <v-card v-else-if="nodeIs('Entity')" flat>
-    <EntityPanel :value="model"></EntityPanel>
-  </v-card>
-  <v-card v-else>
-    <v-card-text>
+  <v-card>
+    <component v-if="component" v-bind:is="component" v-bind:value="model" />
+    <v-card-text v-else>
       Nothing Selected
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-import PaperPanel from "./PaperPanel";
-import AuthorPanel from "./AuthorPanel";
-import GenePanel from "./GenePanel";
-import PatentPanel from "./PatentPanel";
-import AffiliationPanel from "./AffiliationPanel";
-import { isOfType } from "../util/Neo4jGraphBuilder";
-import ProteinPanel from "./ProteinPanel";
-import EntityPanel from "./EntityPanel";
-import PathwayPanel from "./PathwayPanel";
-import TissuePanel from "./TissuePanel";
-import ClinicalTrialPanel from "./ClinicalTrialPanel";
-import DiseasePanel from "./DiseasePanel";
+const ctx = require.context("./", false, /Panel$/);
+
+const components = {};
+ctx.keys().forEach((k) => {
+  const key = k.match(/\.\/(.*Panel)/)[1];
+  if (key) {
+    components[key] = ctx(k).default;
+  }
+});
 
 export default {
   name: "DetailPanel",
-  components: {
-    DiseasePanel,
-    ClinicalTrialPanel,
-    TissuePanel,
-    PathwayPanel,
-    EntityPanel,
-    ProteinPanel,
-    PaperPanel,
-    AuthorPanel,
-    GenePanel,
-    PatentPanel,
-    AffiliationPanel,
-  },
+  components,
   data: () => ({
     model: null,
   }),
   methods: {
-    nodeIs(label) {
-      return isOfType(this.model, label);
-    },
     show(value) {
       this.model = value;
+    },
+  },
+  computed: {
+    component: function () {
+      if (this.model) {
+        if (this.model.labels && this.model.labels.length > 0) {
+          return components[this.model.labels[0] + "Panel"];
+        }
+      }
+      return null;
     },
   },
 };
